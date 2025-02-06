751e409dda20ec31608dc9a324cd0c6d4378d8d6
andzekausk <s23kausandz@venta.lv>
2025-02-06 11:01:50 +0200

    fixed up admin.vue

--- vue-frontend/src/views/Admin.vue
+++ vue-frontend/src/views/Admin.vue
@@ -1,4 +1,28 @@
 <template>
+  <div>
+    <h2>Admin Page</h2>
+    <p v-if="isAdmin">Welcome, Admin!</p>
+    <p v-else>Access denied!</p>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useAuthStore } from "./../store/auth";
+
+export default {
+  setup() {
+    const authStore = useAuthStore();
+
+    // Use computed to make isAdmin reactive
+    const isAdmin = computed(() => authStore.isAdmin);
+
+    return { isAdmin };
+  }
+};
+</script>
+
+<!-- <template>
     <div>
       <h2>Admin Page</h2>
       <p>{{ message }}</p>
@@ -25,4 +49,4 @@
     }
   };
   </script>
-  
+   -->
